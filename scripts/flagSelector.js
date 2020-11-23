e13b28b30f11e1a2bd2f09ada793f2b4ac5d0c6c
Barrett Osmundson <barosmun@cs.du.edu>
2020-11-23 00:41:17 -0500

    hidden flag name

--- scripts/flagSelector.js
+++ scripts/flagSelector.js
@@ -1,15 +1,29 @@
-$(document).ready(function(){
+$(document).ready(randomFlag());
+
+function randomFlag(){
   var name;
   var link;
   var num = Math.floor((Math.random() * 194) + 0);
   console.log("data: ");
-  $.getJSON("scripts/country_flags.json", function(data) {
-    name = data[num].Country;
-    link = data[num].ImageURL;
-    console.log(name);
-    console.log(link);
-    $("#flagName").html(name);
-    $("#flagImage").attr("src", link);
-  });
-
-});
+  // false FOR LOCAL TESTS | true FOR LIVE BUILD
+  if(true){
+    $.getJSON("scripts/country_flags.json", function(data) {
+      name = data[num].Country;
+      link = data[num].ImageURL;
+      console.log(name);
+      console.log(link);
+      $("#flagName").html(name);
+      $("#flagImage").attr("src", link);
+    });
+  }
+  else{
+    $.getJSON("https://barrettosmundson.com/scripts/country_flags.json", function(data) {
+      name = data[num].Country;
+      link = data[num].ImageURL;
+      console.log(name);
+      console.log(link);
+      $("#flagName").html(name);
+      $("#flagImage").attr("src", link);
+    });
+  }
+}
