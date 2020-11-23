2a3a404627f7fd144bf8a214e86265dc99b4c459
Barrett Osmundson <barosmun@cs.du.edu>
2020-11-23 00:21:36 -0500

    quick fix (local JSON, not https)

--- scripts/flagSelector.js
+++ scripts/flagSelector.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
   var link;
   var num = Math.floor((Math.random() * 194) + 0);
   console.log("data: ");
-  $.getJSON("https://barrettosmundson.com/scripts/country_flags.json", function(data) {
+  $.getJSON("scripts/country_flags.json", function(data) {
     name = data[num].Country;
     link = data[num].ImageURL;
     console.log(name);
