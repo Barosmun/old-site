718ec04eff4a38332f27dc48355db80e75e3a914
Barrett Osmundson <barosmun@cs.du.edu>
2021-02-09 23:14:42 -0500

    html preload links

--- scripts/preload.js
+++ scripts/preload.js
@@ -1,30 +1,3 @@
-
-
 $(document).ready(function() {
 	$("body").removeClass("preload");
-	
-	var emailActive = document.createElement("img");
-	var linkedinActive = document.createElement("img");
-	var githubActive = document.createElement("img");
-	var hackerrankActive = document.createElement("img");
-
-	emailActive.onload = function(){
-		document.removeChild(emailActive)
-	}
-	linkedinActive.onload = function(){
-		document.removeChild(linkedinActive)
-	}
-	githubActive.onload = function(){
-		document.removeChild(githubActive)
-	}
-	hackerrankActive.onload = function(){
-		document.removeChild(hackerrankActive)
-	}
-	setTimeout(function(){
-		emailActive.src = "../images/email_active.svg";
-		linkedinActive.src = "../images/linkedin_active.svg";
-		githubActive.src = "../images/github_active.svg";
-		hackerrankActive.src = "../images/hackerrank_active.svg";
-
-	},50);
   });
